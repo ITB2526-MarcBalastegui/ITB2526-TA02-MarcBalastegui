7d451178dd00c94164ebcc78bca6e80980aa567b
marc balastegui <marc.balastegui.7e9@itb.cat>
2025-09-26 13:24:51 +0200

    Canvi

---
+++ TA02/comprovarEdat.py
@@ -0,0 +1,18 @@
+"""
+
+Marc Balastegui Martos
+
+26/09/2025
+
+ASIXc
+
+Descripció: TA02, Projecte de PyCharm de proves
+
+"""
+
+# Programa que demana l'edat i diu si ets major d'edat.
+
+edat=int(input("Quina edat tens?"))
+if edat>=18:print("Ets major d'edat")
+
+print("Programa Finalitzat")
